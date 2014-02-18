80580f317a3d4abde4f562600ec6aa7098809795
Kristian Dyhrberg <kdyhrberg@gmail.com>
2014-02-18 10:48:56 +0100

    Created Snakegame project

---
+++ src/SnakeGame/SnakeGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADIPlus.Drawing;
+
+namespace SnakeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AsciiGraphics g = AsciiGraphics.FromUnManagedConsole())
+                g.DrawPoint(new AsciiPen('*', AsciiColors.Blue), new Point(10,10));
+
+            Console.ReadKey(true);
+        }
+    }
+}
